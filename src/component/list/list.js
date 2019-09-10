bb29a3f4ac75f1ef8fc806b58b056cc03110e682
uzunhair <uzunhair@yandex.ru>
2019-09-10 23:05:34 +0300

    Вывел массив элементов

--- src/component/list/list.js
+++ src/component/list/list.js
@@ -7,30 +7,16 @@ export default class List extends Component {
   render() {
 
     const { todos } = this.props;
-
-    const numbers = [1, 4, 9];
-    const roots = numbers.map(Math.sqrt);
+    const elements = todos.map((item) => {
+      return (
+        <ListItem {...item} />
+      )
+    });
 
     return (
       <div className="card">
         <ul className="list-group list-group-flush">
-          {roots}
-          <ListItem
-            label={todos[1].label}
-            important={todos[1].important}
-          />
-          <ListItem
-            label="List item 3"
-            important="badge-danger"
-          />
-          <ListItem
-            label="List item 4"
-            important="badge-info"
-          />
-          <ListItem
-            label="List item 5"
-            important="badge-info"
-          />
+          {elements}
         </ul>
       </div>
     )
